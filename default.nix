266bb152e13262e9a9cf4a9393be8a5cb3fa4d00
Stefan Junker <mail@stefanjunker.de>
2016-02-07 09:07:14 +0100

    *: set dnsquirks to default
    rkt can now add nameservers via `--dns` flag.

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@
 }:
 
 let 
+  dnsquirks = false;
 in {
   acserver = import pkgs/linux/acserver.nix { inherit pkgs mkACI; static=false; };
   bash = import pkgs/linux/bash.nix { inherit pkgs mkACI; static=false; };
@@ -16,8 +17,11 @@ in {
   coreosIpxeServer = import pkgs/linux/coreos-ipxe-server.nix { inherit pkgs mkACI; };
   dnsmasq = import pkgs/linux/dnsmasq.nix { inherit pkgs mkACI; };
   dnsfail = import pkgs/linux/dnsfail.nix { inherit pkgs mkACI; };
-  docker = import pkgs/linux/docker.nix { inherit pkgs mkACI; dnsquirks=false; };
+  docker = import pkgs/linux/docker.nix { inherit pkgs mkACI; inherit dnsquirks; };
+  dovecot = import pkgs/linux/dovecot.nix { inherit pkgs mkACI; inherit dnsquirks; };
   etcd2 = import pkgs/linux/etcd2.nix { inherit pkgs mkACI; };
+  getmail = import pkgs/linux/getmail.nix { inherit pkgs mkACI; inherit dnsquirks; };
+  iperf = import pkgs/linux/iperf.nix { inherit pkgs mkACI; static=false; };
   pixiecore = import pkgs/linux/pixiecore.nix { inherit pkgs mkACI; static=false; };
   qemu = import pkgs/linux/qemu.nix { inherit pkgs mkACI; };
   rkt = import pkgs/linux/rkt.nix { inherit pkgs mkACI; dnsquirks=false;};
