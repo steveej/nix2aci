2b4be0801a8050e2e1806050fe3dca5282ba292c
Stefan Junker <mail@stefanjunker.de>
2016-02-07 09:05:17 +0100

    linux/getmail: init

---
+++ pkgs/linux/getmail.nix
@@ -0,0 +1,20 @@
+{ mkACI, pkgs, thin ? false, ... } @ args:
+let 
+  pkg = pkgs.getmail;
+in
+
+mkACI rec {
+  inherit pkgs;
+  inherit thin;
+  dnsquirks = args.dnsquirks;
+
+  packages = [ pkg pkgs.busybox pkgs.msmtp pkgs.python27Packages.supervisor ];
+
+  user = "1000";
+  group = "1000";
+
+  env = {
+      "LC_ALL" = "en_US.UTF-8";
+      "LANG" = "en_US.UTF-8";
+  };
+}
