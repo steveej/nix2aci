57dec00f48b980d4c92e1ec03f49646ddeffb1d4
Stefan Junker <mail@stefanjunker.de>
2016-02-07 09:04:41 +0100

    linux/iperf: init

---
+++ pkgs/linux/iperf.nix
@@ -0,0 +1,30 @@
+{ mkACI
+, pkgs
+, thin ? false
+, static
+, ... }
+@ args:
+
+let
+  pkg = pkgs.iperf;
+  busybox = if static == true
+    then
+      (pkgs.busybox.override {
+         extraConfig = ''
+           CONFIG_STATIC y
+           CONFIG_INSTALL_APPLET_DONT y
+           CONFIG_INSTALL_APPLET_SYMLINKS n
+         '';
+      })
+    else pkgs.busybox;
+in
+
+mkACI rec {
+  inherit pkgs;
+  inherit static;
+  thin = false;
+  packages = [ pkg busybox pkgs.eject ];
+  versionAddon = if static == true then "-static" else "";
+  exec = ''/bin/busybox -- sh -c "busybox mkdir -p /sbin; /bin/busybox --install -s; sh"'';
+}
+
